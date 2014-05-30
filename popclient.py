d6521e93121f8381372eff3b93c11df13b6a3a9e
Robert Freiberger <rfreiberger@gmail.com>
2014-05-29 22:14:19 -0700

    Fixes from Louis.
    Louis added the corrections with the name of the functions.

--- popclient.py
+++ popclient.py
@@ -9,14 +9,17 @@ import textwrap
 import argparse
 from argparse import RawTextHelpFormatter
 from email.parser import Parser
-parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawDescriptionHelpFormatter, 
+import pprint
+pp = pprint.PrettyPrinter(indent=4, depth=6)
+
+email_parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
         description=textwrap.dedent('''\
         **********************************************************
-        * Basic POP client for testing mail from command line.   * 
-        *                                                        * 
-        * You can also use the verbose option (either 1 or 2)    * 
-        * for more logging detail. Default is debug 0.           * 
+        * Basic POP client for testing mail from command line.   *
+        *                                                        *
+        * You can also use the verbose option (either 1 or 2)    *
+        * for more logging detail. Default is debug 0.           *
         *                                                        *
         * Examples:                                              *
         * $ python pop_client.py <user> <pass> <server>          *
@@ -24,14 +27,11 @@ parser = argparse.ArgumentParser(
         *                                                        *
         **********************************************************
         '''))
-parser.add_argument("user", help="Account username")
-parser.add_argument("password", help="Account password")
-parser.add_argument("server", help="POP server")
-# parser.add_argument("-v", "--verbose", action="count", default=0, help="Verbose output")
-parser.add_argument("-v", "--verbose", default=0, type=int, choices=[1, 2], help="Verbose output")
-args = parser.parse_args()
-
-
+email_parser.add_argument("user", help="Account username")
+email_parser.add_argument("password", help="Account password")
+email_parser.add_argument("server", help="POP server")
+email_parser.add_argument("-v", "--verbose", default=0, type=int, choices=[1, 2], help="Verbose output")
+args = email_parser.parse_args()
 
 def returnPop(user,password,server,verbose):
     pop3_mail = poplib.POP3_SSL(server)
@@ -43,17 +43,18 @@ def returnPop(user,password,server,verbose):
     currentMail, totalSize = (pop3_stat)
     print "Total New Mail: %s (size %s bytes)" % pop3_stat
     latest_email = pop3_mail.retr(currentMail)
-    # print latest_email
-    # emailMessage = str(latest_email)
-    # print emailMessage
-    #msg = email.message_from_string(emailMessage)
-    # print msg['from']
-    # print msg['to']
-    # newMail.join(latest_email)
-    newMail = str(latest_email)
-    headers = Parser().parsestr(newMail)
-    print 'To: %s' % headers['to']
-    print 'From: %s' % headers['from']
-    print 'Subject: %s' % headers['subject']
-    
+    pop3_mail.quit()
+
+    # pp.pprint(latest_email)
+
+    latest_email = "\n".join(latest_email[1])
+    latest_email = email.parser.Parser().parsestr(latest_email)
+
+
+
+    print 'To: %s' % latest_email['to']
+    print 'From: %s' % latest_email['from']
+    print 'Subject: %s' % latest_email['subject']
+    print 'Date: %s' % latest_email['date']
+
 print returnPop(args.user, args.password, args.server, args.verbose)
